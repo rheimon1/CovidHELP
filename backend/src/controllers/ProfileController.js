014938a3e648a8af967f622340bd606384757690
rheimon1 <wallaceramonns.dev@usp.br>
2020-05-25 02:18:37 -0300

    remove some functionalities

--- backend/src/controllers/ProfileController.js
+++ backend/src/controllers/ProfileController.js
@@ -3,12 +3,9 @@ const connection = require('../database/connection');
 module.exports = {
   async index(request, response) {
     //const user_id = request.headers['authorization'];
-    const dataUser = response.getHeader('x-access');
-    
-    const user = dataUser.sub;
 
     const incidents = await connection('incidents')
-      .where('user_id', user.id)
+      //.where('user_id', user.id)
       .select('*')
 
     return response.json(incidents);
