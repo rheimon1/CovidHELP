281f76c841060cadb5104df256325d93f862e87f
rheimon1 <wallaceramonns.dev@usp.br>
2020-06-01 15:43:41 -0300

    add app.js

---
+++ backend/src/app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors = require('cors');
+const routes = require('./routes');
+const { errors } = require('celebrate');
+require('dotenv/config');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(routes);
+app.use(errors());
+
+module.exports = app;
