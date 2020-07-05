cd26206af91e9f030188d7b42032ed3ea12b7cef
Wallace Ramon Nogueira Soares <49095811+rheimon1@users.noreply.github.com>
2020-07-05 16:48:04 -0300

    Delete api.js

--- mobile/src/services/api.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: 'https://rest-api-fsiproject.herokuapp.com'
-});
-
-//https://rest-api-fsiproject.herokuapp.com:80
-
-export default api;
